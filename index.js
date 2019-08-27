bd906cf932afee2698caf7a88d66406a1ab08e90
Liz Burton <liz.burton147@gmail.com>
2019-08-27 22:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(start, stop) {
+  return Math.abs(start - stop) * 264;
+}
+
+function calculatesFarePrice(start, stop) {
+  const distance = distanceTravelledInFeet(start, stop);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) * .02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far"
+  }
+  
+}
